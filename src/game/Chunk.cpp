86049d8948ee9b18c9243ff59b8ed7dfd4d4cf70
GrispenX <145205694+GrispenX@users.noreply.github.com>
2026-02-27 06:55:33 +0200

    Add unsaved changes for previous commit

--- src/game/Chunk.cpp
+++ src/game/Chunk.cpp
@@ -1,6 +1,6 @@
 #include "game/Chunk.h"
 
-Chunk::Chunk(std::array<std::unique_ptr<ICell>, GameConf::CHUNK_WIDTH * GameConf::CHUNK_HEIGHT> cells) :
+Chunk::Chunk(std::array<std::array<std::unique_ptr<ICell>, GameConf::CHUNK_WIDTH>, GameConf::CHUNK_HEIGHT> cells) :
     m_Cells(std::move(cells))
 {
 
@@ -8,15 +8,15 @@ Chunk::Chunk(std::array<std::unique_ptr<ICell>, GameConf::CHUNK_WIDTH * GameConf
 
 SweepResult Chunk::Sweep(const CellInChunkPosition& cell_pos)
 {
-    return m_Cells[cell_pos.Col * GameConf::CHUNK_HEIGHT + cell_pos.Row]->Sweep();
+    return m_Cells[cell_pos.Row][cell_pos.Col]->Sweep();
 }
 
 FlagResult Chunk::Flag(const CellInChunkPosition& cell_pos)
 {
-    return m_Cells[cell_pos.Col * GameConf::CHUNK_HEIGHT + cell_pos.Row]->Flag();
+    return m_Cells[cell_pos.Row][cell_pos.Col]->Flag();
 }
 
 const ICell* Chunk::GetCell(const CellInChunkPosition& cell_pos) const
 {
-    return m_Cells[cell_pos.Col * GameConf::CHUNK_HEIGHT + cell_pos.Row].get();
+    return m_Cells[cell_pos.Row][cell_pos.Col].get();
 }
