a74c44a9ae53f8659d22eacf86df1789f44a816e
GrispenX <145205694+GrispenX@users.noreply.github.com>
2026-03-07 17:08:19 +0200

    Remove template from chunk generator

--- include/game/IChunkGenerator.h
+++ include/game/IChunkGenerator.h
@@ -5,13 +5,12 @@
 #include "game/GeometryConcept.h"
 #include <memory>
 
-template<Geometry TGeometry>
 class IChunkGenerator
 {
 public:
     ~IChunkGenerator() = default;
 
-    virtual std::shared_ptr<IChunk> GenerateChunk(const TGeometry::ChunkPosition& chunk_pos) = 0;
+    virtual std::shared_ptr<IChunk> GenerateChunk(size_t chunk_pos_hash, size_t cells_amount) = 0;
 };
 
 #endif // INCLUDE_GAME_ICHUNKGENERATOR_H_
