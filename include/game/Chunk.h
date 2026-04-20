4122ebe6b70c779e7344c89e8d3272af5fd1be28
GrispenX <145205694+GrispenX@users.noreply.github.com>
2026-04-20 06:16:42 +0300

    Delete old commented code

--- include/game/Chunk.h
+++ include/game/Chunk.h
@@ -13,33 +13,18 @@ public:
     using CellPosition = typename T::CellPosition;
 
     Chunk(std::unordered_map<CellPosition, std::unique_ptr<ICell>> cells) :
-        // m_Cells(cells.size())
-        m_Cells(std::move(cells))
-    {
-        // for(auto& [pos, cell] : cells)
-        // {
-        //     size_t index = CellPosToIndex(pos);
-        //     if(m_Cells.capacity() <= index) m_Cells.resize(index + 1);
-        //     m_Cells.at(CellPosToIndex(pos)) = std::move(cell);
-        // }
-    }
+        m_Cells(std::move(cells)) {}
 
     SweepResult Sweep(const CellPosition& pos) override
     {
-        // size_t index = CellPosToIndex(pos);
-        // return m_Cells[index]->Sweep();
         return m_Cells[pos]->Sweep();
     }
     FlagResult Flag(const CellPosition& pos) override
     {
-        // size_t index = CellPosToIndex(pos);
-        // return m_Cells[index]->Flag();
         return m_Cells[pos]->Flag();
     }
     ICell* GetCell(const CellPosition& pos) override
     {
-        // size_t index = CellPosToIndex(pos);
-        // return m_Cells[index].get();
         return m_Cells[pos].get();
     }
     size_t GetSize() const override
@@ -48,9 +33,6 @@ public:
     }
 
 private:
-    // size_t CellPosToIndex(const CellPosition& pos);
-
-    // std::vector<std::unique_ptr<ICell>> m_Cells;
     std::unordered_map<CellPosition, std::unique_ptr<ICell>> m_Cells;
 };
 
